d1a0de546ddf13afc48b73ec013fcaa5c267d0c7
Patrick Waters <patrick.waters@liftoff.io>
2020-10-04 10:53:30 -0700

    Move files

--- front/utils.js
+++
@@ -1,9 +0,0 @@
-export function range (i,j) {
-  return (j
-          ? Array(j-i).fill(0).map( (_, idx) => idx+i )
-          : Array(i).fill(0).map( (_, idx) => idx ) );
-}
-
-export function deepCopy(gameState) {
-  return JSON.parse(JSON.stringify(gameState));
-}
